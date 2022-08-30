75123860f37a3ae88dda4075cabbfe17ff198e64
Wannaporn D <wannapornj.dea@gmail.com>
2022-08-30 23:41:40 +0700

    style: logo and input component

---
+++ pages/components/input.jsx
@@ -0,0 +1,17 @@
+const StyledInput = (props) => {
+    return (
+        <div className='flex flex-col items-start w-full'>
+            <label className='p-0 text-sm text-gray-200'>{props.title}</label>
+            <input
+                ref={props.refs}
+                className='w-full h-11 rounded bg-offWhite border border-gray-200 pl-3'
+                type={props.type}
+                value={props.value}
+                placeholder={props.placeholder}
+                onChange={(e) => props.change(e)}
+            />
+        </div>
+    )
+}
+
+export default StyledInput
