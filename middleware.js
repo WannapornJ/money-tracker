ed2419630548985eb457e0ac5572d4a77458a27e
Wannaporn D <wannapornj.dea@gmail.com>
2022-09-08 22:06:13 +0700

    chore : rename cookie object, change local storage to function

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export const middleware = (req) => {
-  let verify = req.cookies.get('SIGN_IN');
+  let verify = req.cookies.get('USER');
   let url = req.url;
   let privatePages = ['http://localhost:3000/', 'http://localhost:3000/category'];
   let publicPages = ['http://localhost:3000/signin', 'http://localhost:3000/sigup']
