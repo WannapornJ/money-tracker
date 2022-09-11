01bf53732591ff68fd4020ec1949e4f6d3ebda11
Wannaporn D <wannapornj.dea@gmail.com>
2022-09-12 02:56:14 +0700

    fix: token source

--- pages/utilities/loacalStorageServices.js
+++ pages/utilities/loacalStorageServices.js
@@ -1,3 +1,5 @@
+import { decode } from "jsonwebtoken";
+
 export const setToken = (token) => {
   localStorage.setItem('ACC_TOKEN', token);
 };
@@ -8,3 +10,10 @@ export const getToken = () => {
 export const removeToken = () => {
     localStorage.removeItem('ACC_TOKEN');
 };
+
+export const getUser = () => {
+  const token = getToken();
+  if(token){
+    return decode(token).username
+  }
+}
